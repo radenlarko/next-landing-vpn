a2fce662a7d4e70bf4bd4eb40d2d41cd82c79204
radenlarko <radenlarko@gmail.com>
2022-06-23 12:03:12 +0700

    Add component Hero

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import Head from "next/head";
 // import Feature from "../components/Feature";
 // import Pricing from "../components/Pricing";
-// import Hero from "../components/Hero";
+import Hero from "../components/Hero";
 import Layout from "../components/Layout/Layout";
+
+const listUser = [
+  {
+    name: "Users",
+    number: "390",
+    icon: "/assets/Icon/heroicons_sm-user.svg",
+  },
+  {
+    name: "Locations",
+    number: "20",
+    icon: "/assets/Icon/gridicons_location.svg",
+  },
+  {
+    name: "Server",
+    number: "50",
+    icon: "/assets/Icon/bx_bxs-server.svg",
+  },
+];
 
 export default function Home() {
   return (
@@ -12,8 +30,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        {/* <Hero />
-        <Feature />
+        <Hero listUser={listUser} />
+        {/* <Feature />
         <Pricing /> */}
       </Layout>
     </>
